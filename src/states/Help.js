efcbc40dd07748cc5a876788882dbf8731d0dc79
Tanya2501 <tanya.bahmatovich@tut.by>
2017-04-11 20:33:11 +0200

    game/fix: fixed little mistake

--- src/states/Help.js
+++ src/states/Help.js
@@ -8,8 +8,7 @@ export default class extends Phaser.State {
         const background = this.add.sprite(this.world.centerX - 150, 50, 'menu');
         background.scale.setTo(0.5, 0.5);
 
-        this.add.text(worldCenterX - 120, 150, 'Arrow keys (LEFT, RIGHT, UP) -\n Move and jump', config.textStyle);
-        this.add.text(worldCenterX - 120, 190, 'Arrow key DOWN - Shoot your item', config.textStyle);
+        this.add.text(worldCenterX - 120, 150, 'Arrow keys (LEFT, RIGHT, UP) -\n Move, jump and shoot', config.textStyle);
 
         const controlsButton = this.game.add.button(worldCenterX - 120, 350, 'menuTab', () => {
             this.state.start('Menu');
